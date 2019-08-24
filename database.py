bc2aeca628c18b2ea0501881c0b7f61161e74b5b
EricPeter <kigaye.ericpeter@yahoo.com>
2019-08-24 13:10:49 +0300

    eleven commit

--- database.py
+++ database.py
@@ -3,12 +3,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 import psycopg2
 import os 
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+#SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 # SECRET_KEY = os.environ.get('SECRET_KEY')
 # SQLALCHEMY_TRACK_MODIFICATIONS = False
-DATABASE_URL = 'sqlite:///database.db'
+#DATABASE_URL = 'sqlite:///database.db'
 
-engine = create_engine('SQLALCHEMY_DATABASE_URI ', convert_unicode=True)
+engine = create_engine('DATABASE_URL', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
