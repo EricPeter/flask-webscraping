532c21ce5081c986fed67d6b4c5e1f8f77049a60
EricPeter <kigaye.ericpeter@yahoo.com>
2019-08-26 08:57:59 +0300

    added the database

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ import os
 #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 # SECRET_KEY = os.environ.get('SECRET_KEY')
 # SQLALCHEMY_TRACK_MODIFICATIONS = False
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
 engine = create_engine('DATABASE_URL', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
