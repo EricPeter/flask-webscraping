9851de75fb26a3fabe847e0ee61cc25fcc21d212
EricPeter <kigaye.ericpeter@yahoo.com>
2019-08-26 09:44:14 +0300

    added sqlite commit

--- database.py
+++ database.py
@@ -1,14 +1,13 @@
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-import psycopg2
 import os 
 #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 # SECRET_KEY = os.environ.get('SECRET_KEY')
 # SQLALCHEMY_TRACK_MODIFICATIONS = False
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-DATABASE_URL = 'sqlite:///db.sqlite3'
-engine = create_engine(DATABASE_URL, convert_unicode=True)
+#DATABASE_URL = 'sqlite:///db.sqlite3'
+engine = create_engine('DATABASE_URL', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
